a7eeb7ed4892fc7e7706a4cbfa35da35bf867beb
L.M. (CareF) <lm0329@126.com>
2015-12-12 01:44:50 +0800

    collisions for sphere

--- reference/PhysicalEngine/p5out/src/physics/collisions.cpp
+++ reference/PhysicalEngine/p5out/src/physics/collisions.cpp
@@ -5,6 +5,16 @@ namespace _462 {
 bool collides( SphereBody& body1, SphereBody& body2, real_t collision_damping )
 {
     // TODO detect collision. If there is one, update velocity
+	if ((body1.radius + body2.radius) * body1.radius + body2.radius
+		>= squared_length(body1.position - body2.position)) {
+		//collision happens
+		Vector3 totalMomentum = body1.velocity * body1.mass + body2.velocity * body2.mass;
+		Vector3 velocityDif = body1.velocity - body2.velocity;
+		real_t totalMass = body1.mass + body2.mass;
+		body1.velocity = (totalMomentum - velocityDif*collision_damping*body2.mass) / totalMass;
+		body2.velocity = (totalMomentum + velocityDif*collision_damping*body1.mass) / totalMass;
+		return true;
+	}
 
     return false;
 }
